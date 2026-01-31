3a346a7a6b90ac3a17be5aa857fa05958c01654a
sameer bhatti <100254115+bhatti8162@users.noreply.github.com>
2026-01-31 09:31:33 +0500

    Projects

---
+++ userAccountsApiProject04/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+# urlpatterns = [
+#     path('public/', views.public_view, name='public_view'),
+#     path('private/', views.private_view, name='private_view'),
+# ]
+
+# urlpatterns = [
+#     path('blogs/', views.blog_list, name='blog_list'),
+# ]
+
+
+urlpatterns = [
+    path('get-token/', obtain_auth_token, name='api_token_auth'),
+    path('profile/', views.user_profile, name='user_profile'),
+    path('admin-panel/', views.admin_panel, name='admin_panel'),
+]
